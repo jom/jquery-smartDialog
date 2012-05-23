ae5c5f3c7085e3ad0c3c755036d20568c10b4d60
unknown <WebAdmin@.(none)>
2012-05-22 18:11:25 -0700

    dialog load fail

--- src/jquery.smartDialog.js
+++ src/jquery.smartDialog.js
@@ -130,6 +130,9 @@
 						$("#"+ self.options.dialog.id +" ~ .ui-dialog-buttonpane button").button("enable");
 					}
 				};
+				self.options.dialog.content.ajaxOptions.error = function(data) {
+					$("#"+ self.options.dialog.id).dialog("close");
+				};
 				$("#"+ self.options.dialog.id).html(self.options.dialog.content.loadingMessage);
 				$("#"+ self.options.dialog.id +" ~ .ui-dialog-buttonpane button").button("disable");
 				$.ajax(url, self.options.dialog.content.ajaxOptions);
